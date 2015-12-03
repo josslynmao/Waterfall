156c7c619b38f6f7054ebca95dbd6d01c90aab57
josslynmao <mao.huifeng@hotmail.com>
2015-12-03 20:26:39 +0800

    height

--- 瀑布流.js
+++ 瀑布流.js
@@ -1,18 +1,27 @@
 function findPosition(){
 	var picbox = document.getElementsByClassName("picbox");
 	var colNum = parseInt(document.documentElement.clientWidth/picbox[0].offsetWidth);
-	console.log(colNum);
+	console.log(colNum);//检验
 	var container = document.getElementById("container");
-	container.style.width = (picbox[0].offsetWidth + 2 * 5) * colNum + "px";//2*5是margin
-	console.log(container.style.width);
+	container.style.width = (picbox[0].offsetWidth + 2 * 5) * colNum + "px";//用js的方法获取每个图片的宽度，设置container宽度，从而拖动浏览器不会改变float每行图片列数。2*5是margin。
+	console.log(container.style.width);//检验
 	var pic = document.getElementsByClassName("pic");
-	var height = [];
-	for (var i = 0; i < pic.length; i++) {
+	var height = [];//用来记录四列总高度变化
+	for (var i = 0; i < colNum; i++) {
 		height[i] = pic[i].offsetHeight;
-		console.log(height[i]);
+		console.log(height[i]);//检验
 	}
 	var min = minHeight(height);
-	console.log(min);
+	console.log(min);//检验
+	for (var i = 0; i < picbox.length; i++) {
+		if(i > colNum){
+			picbox[i].style.position = "absolute";
+			picbox[i].style.top = min;
+			picbox[i].style.left = (i%colNum) * picbox[i].offsetWidth;
+			height[i%colNum] = picbox[i].offsetHeight + min;
+		}
+	}
+
 }
 
 window.onload = findPosition;
