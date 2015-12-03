b656871930caac11038951cb14024692b15ee7a9
josslynmao <mao.huifeng@hotmail.com>
2015-12-03 19:05:55 +0800

    ~~~

--- 瀑布流.js
+++ 瀑布流.js
@@ -1,3 +1,14 @@
-function findPosition(){}
+function findPosition(){
+	var picbox = document.getElementsByClassName("picbox");
+	var colNum = document.documentElement.clientWidth/picbox[0].offsetWidth;
+	console.log(colNum);
+	var pic = document.getElementsByClassName("pic");
+	for (var i = 0; i < pic.length; i++) {
+		pic[i].height = pic[i].offsetHeight;
+		console.log(pic[i].height);
+	}
+}
+
+window.onload = findPosition;
 
 function minHeight(){}
