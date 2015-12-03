9a052baea7a9556b8989c43fab8ab4f53e786bff
josslynmao <mao.huifeng@hotmail.com>
2015-12-03 19:56:39 +0800

    改错误

--- 瀑布流.js
+++ 瀑布流.js
@@ -1,14 +1,23 @@
 function findPosition(){
 	var picbox = document.getElementsByClassName("picbox");
-	var colNum = document.documentElement.clientWidth/picbox[0].offsetWidth;
+	var colNum = parseInt(document.documentElement.clientWidth/picbox[0].offsetWidth);
 	console.log(colNum);
+	var container = document.getElementById("container");
+	container.style.width = (picbox[0].offsetWidth + 2 * 5) * colNum + "px";//2*5是margin
+	console.log(container.style.width);
 	var pic = document.getElementsByClassName("pic");
+	var height = [];
 	for (var i = 0; i < pic.length; i++) {
-		pic[i].height = pic[i].offsetHeight;
-		console.log(pic[i].height);
+		height[i] = pic[i].offsetHeight;
+		console.log(height[i]);
 	}
+	var min = minHeight(height);
+	console.log(min);
 }
 
 window.onload = findPosition;
 
-function minHeight(){}
+function minHeight(picHeightArray){
+	var minHeight = Math.min.apply(Math,picHeightArray);//这个math！！终于改对了。。。
+	return minHeight;
+}
