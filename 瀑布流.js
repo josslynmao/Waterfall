c7edde57ff23a45e15cf1b7ad289d15756948be1
josslynmao <mao.huifeng@hotmail.com>
2015-12-04 22:16:15 +0800

    111

--- 瀑布流.js
+++ 瀑布流.js
@@ -2,21 +2,17 @@ function findPosition(){
 	var picbox = document.getElementsByClassName("picbox");
 	var container = document.getElementById("container");
 	var pic = document.getElementsByClassName("pic");
-
 	var colNum = parseInt(document.documentElement.clientWidth/picbox[0].offsetWidth);
 	console.log(colNum);//检验
 	container.style.width = (picbox[0].offsetWidth) * colNum + "px";//用js的方法获取每个图片的宽度，设置container宽度，从而拖动浏览器不会改变float每行图片列数。2*5是margin。
 	console.log(container.style.width);//检验
-
 	var height = [];//用来记录四列总高度变化
 	for (var i = 0; i < colNum; i++) {
 		height[i] = picbox[i].offsetHeight;
 	}
 	console.log(height);//检验
-
 	var min = height[minHeight(height)];
 	console.log(min);//检验
-
 	var heightColNum = minHeight(height);
 	for (var i = 0; i < picbox.length; i++) {
 		heightColNum = minHeight(height);
@@ -35,7 +31,6 @@ function findPosition(){
 
 }
 
-
 function addPic(){
 	var picbox = document.getElementsByClassName("picbox");
 	var lastpic = picbox[picbox.length-1];
@@ -48,9 +43,12 @@ function addPic(){
 		console.log("onload pic");
 		return true;
 	}
-
 }
 
+function minHeight(picHeightArray){
+	var minHeight = Math.min.apply(Math,picHeightArray);//这个math！！终于改对了。。。
+	return picHeightArray.indexOf(minHeight);
+}
 
 window.onload = function(){
 	findPosition();
@@ -75,9 +73,3 @@ window.onload = function(){
 		};
 	}
 }
-
-function minHeight(picHeightArray){
-	var minHeight = Math.min.apply(Math,picHeightArray);//这个math！！终于改对了。。。
-	return picHeightArray.indexOf(minHeight);
-}
-
